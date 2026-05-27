42ec5eda4b79ce980858f66c987bbcc96290bead
SakamakiShota <79351487+makimaki510151@users.noreply.github.com>
2026-05-27 13:25:34 +0900

    いい感じ

--- js/unityroomConfig.js
+++ js/unityroomConfig.js
@@ -1,7 +1,7 @@
 /* unityroom ダッシュボードのスコアボード一覧で確認したボード No を設定してください */
 window.LABYRINTH_UNITYROOM = {
     hmacKey:
-        'UUxMUGZlL8JrkdtgDpLNXKakrmGyuHC5XwvJ5iWCc1zAr9me0iW16pNA3L5kAW/H+GuYCQAjqWMXtBgrI9cJGw==',
+        '1ciXdSrLHerMrxsRTtaiov/6HLNkIfR0BRNsNFFVnN8sgwwkvYPlwHEJ1BXmP80e2J1rE3eeQjeVA9Ha29YlLg==',
     boards: {
         clearedStage: 1,
         newPathCells: 2,
